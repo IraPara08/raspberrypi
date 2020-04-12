cf86ef9496b2c7933da26a93124bcc8195e0f732
IraPara08 <meghavellorerao@gmail.com>
2020-04-12 16:56:26 +0000

    First Code Check in for Megha

---
+++ RaspberryPi.py
@@ -0,0 +1,7 @@
+print("My name is Megha!")
+print("I am 11 years old!")
+print("I am in 6th Grade!")
+print("My favorite sport to watch is football and my favorite sport to play is tennis!")
+print("I want a golden retriever as a pet!")
+your_name = input("What is your name?")
+print("You are amazing,", your_name)
