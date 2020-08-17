3e84c551336deffe089dec48e1722150a3ee9e36
IraPara08 <meghavellorerao@gmail.com>
2020-08-17 10:16:21 -0400

    Learnt Input Command in Python

---
+++ survey.py
@@ -0,0 +1,12 @@
+firepitresponse = input("What kind of fire pit would you like, your options are A) Square B) Round? : ")
+print(firepitresponse)
+if firepitresponse == 'A':
+        print('you selected Square Firepit')
+elif firepitresponse == 'B':
+	print('you selected Round Firepit')
+structureresponse  = input("Do you want a structure built into the house or seperate, your options are A) Yes B) No:")
+print(structureresponse)
+if structureresponse == 'A':
+        print('You selected attached structure.')
+elif structureresponse == 'B':
+        print('You selected freestanding patio.')
