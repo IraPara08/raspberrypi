cd6ebad594cedf5aff1d85ca7d75d1e8c54d0dad
IraPara08 <meghavellorerao@gmail.com>
2020-08-18 20:59:58 -0400

    String Operations

---
+++ basicstringoperations.py
@@ -0,0 +1,18 @@
+astring = "My name is Guru Rao!"
+mstring = "My name is Megha Rao!"
+
+print(len(mstring))
+
+#number of characters
+print(len(astring))
+
+#postion of first e
+print(astring.index("u"))
+
+#count of m
+print(astring.count("R"))
+
+#character 3 to 9
+print(astring[3:9])
+
+#ta-da!
